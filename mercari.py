aa1d12cbd71bea435fd4feaa3860c7f48a46351e
Jade <35437313+JadeTank@users.noreply.github.com>
2024-11-26 05:40:34 -0500

    Update JSON Parsing to HTML Parsing

--- mercari.py
+++ mercari.py
@@ -1,48 +1,41 @@
 import requests, json
 from logging import info
-from xml.dom.minidom import parseString
+from bs4 import BeautifulSoup
 from lightbulb import BotApp
 from hikari import Embed, Color
 
 
 async def check_mercari(bot: BotApp, alert: dict) -> None:
-    res = requests.post(
+    res = requests.get(
         f"https://zenmarket.jp/en/mercari.aspx/getProducts?q={alert['name']}&sort=new&order=desc",
-        json={"page": 1},
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        },
+        headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
     )
 
-    content = json.loads(res.json()["d"])
+    items = BeautifulSoup(res.content, 'html.parser').find_all('a', class_='product-item')
 
-    for item in content["Items"]:
-        if bot.d.synced.find_one(name=item["ItemCode"]):
+    for item in items:
+        itemCode=str(item['href']).split('itemCode=')[1]
+        image = item.find('img')['src']
+        price = item.find('span', class_='amount')['data-usd']
+
+        if bot.d.synced.find_one(name=itemCode):
             info("[mercari] already synced — up to date")
             continue
 
         embed = Embed()
         embed.color = Color(0x09B1BA)
-        embed.title = item["ClearTitle"] or "Unknown"
+        embed.title = item.find('h3',class_='item-title')['title'] or "Unknown"
 
-        if item["ItemCode"]:
-            embed.url = (
-                "https://zenmarket.jp/fr/mercariproduct.aspx?itemCode="
-                + item["ItemCode"]
-            )
+        if itemCode:
+            embed.url = "https://zenmarket.jp/en/mercariproduct.aspx?itemCode=" + itemCode + "&skipqproc=true"
 
-        if item["PreviewImageUrl"]:
-            embed.set_image(item["PreviewImageUrl"])
+        if image:
+            embed.set_image(image)
 
-        if item["PriceTextControl"]:
-            try:
-                dom = parseString(item["PriceTextControl"])
-                price = dom.getElementsByTagName("span")[0].getAttribute("data-eur")
-                embed.add_field("Price", price)
-            except:
-                pass
+        if price:
+            embed.add_field("Price", price)
 
-        embed.set_footer(f"Source: Mercari — #{item['ItemCode']}")
+        embed.set_footer(f"Source: Mercari — #{itemCode}")
 
         await bot.rest.create_message(alert["channel_id"], embed=embed)
-        bot.d.synced.insert({"name": item["ItemCode"]})
+        bot.d.synced.insert({"name": itemCode})
